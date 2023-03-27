a58b01137e7b340b0849dd2151b7304f49eaa510
Илья <socspec@yandex.ru>
2023-03-27 20:07:33 +0300

    1

---
+++ test_item.py
@@ -0,0 +1,19 @@
+import time
+import pytest
+
+from selenium.webdriver.common.by import By
+
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/'
+
+def test_presence_cart_button(browser):
+    browser.implicitly_wait(5)
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    time.sleep(10)
+
+    button_name = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket').text
+    
+
+if __name__ == "__main__":
+    pytest.main()
